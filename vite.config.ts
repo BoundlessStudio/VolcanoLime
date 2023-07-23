ebfcb85ab7774e0f44ae98eee39caeb6b2432769
RGBKnights <master@rgbknights.com>
2023-07-23 18:32:09 -0400

    chuck limits and  dynamic import() wtf is this...?

--- vite.config.ts
+++ vite.config.ts
@@ -12,14 +12,6 @@ export default defineConfig({
     }
   },
   build: {
-    rollupOptions: {
-        output:{
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                }
-            }
-        }
-    }
+    chunkSizeWarningLimit: 99999999,
   }
 })
