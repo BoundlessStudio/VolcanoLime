0413d6d636bc2740edfefa0cc1c1da0d0062ce57
RGBKnights <master@rgbknights.com>
2023-07-27 20:20:53 -0400

    change main image to remove Dalle

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -239,7 +239,7 @@ const navigation = {
             class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4"
           >
             <img
-              src="https://i.imgur.com/QsUZIiu.png"
+              src="https://i.imgur.com/QNWvVfG.png"
               alt="App screenshot"
               width="2432"
               height="1442"
